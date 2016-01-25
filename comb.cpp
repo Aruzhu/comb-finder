04a9625b984fdd011040511803018cd84b3e2ff4
Jon Elias <jon39334@gmail.com>
2016-01-25 20:52:11 +0100

    getting input

--- comb.cpp
+++ comb.cpp
@@ -1,5 +1,32 @@
 // all combination finder without laying back each element
 #include <iostream>
+#include <string>
+#include
 using namespace std;
+int occurances(string subject, string totest){
+	int occs = 0;
+	for(int i = 0; i<subject.size(); i++){
+		if(totest == subject.at(i)){
+			occs += 1;
+		}
+	}
+	return occs;
+}
 
-
+int main(){
+	//get user input
+	cout << "write numbers to use (seperate with , and without spaces): ";
+	string ElementInput;
+	getline(cin, ElementInput);
+	cin.clear();
+	
+	int Elements[ occs(ElementInput, ",") ];
+	int ElementsIter = 0;
+	for(int i = 0; i<ElementInput.size(); i++){
+		string letter = ElementInput.at(i);
+		
+		if(letter != ","){
+			Elements[ElementsIter] = 	
+		}
+	}
+}
